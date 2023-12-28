6db13e20313551c14a1a43dc76d90dc2787a3e7b
shon <shoniki951@gmail.com>
2023-12-28 20:29:44 +0200

    SearchError Test

--- testNegativeUser/test_SearchError.py
+++ testNegativeUser/test_SearchError.py
@@ -1,6 +1,4 @@
 import time
-
-from selenium.common import UnexpectedAlertPresentException
 from selenium.webdriver.common.by import By
 from pageObjectsUser.LoginPageUser import LoginUser
 from utilities.readProperties import ReadConfig
@@ -17,32 +15,27 @@ class Test_Negative015_SearchError:
         self.driver = setup
         self.driver.get(self.userloginURL)
         self.driver.maximize_window()
+        time.sleep(2)
 
         self.logger.info("******** Starting Search Error Test *********")
 
         self.lu = LoginUser(self.driver)
-        # self.lu.clickSearch()
-
+        self.lu.clickSearch()
+        time.sleep(2)
         self.logger.info("******** search error validation *********")
 
-        # self.err = self.driver.find_element(By.ID, "Email-error")
-
-        # self.message_element = self.driver.find_element(By.XPATH, "//*[@id='small-search-box-form']/button")
-        # self.accept_button_message = self.driver.execute_script("return arguments[0].validationMessage", self.message_element)
-        # print("Message : ", self.accept_button_message)
-        try:
-            self.lu.clickSearch()
-        except UnexpectedAlertPresentException as e:
-            self.logger.info('[!] Error: ' + str(e))
-
-        # if self.message_element:
-        #     assert True == True
-        #     self.logger.info("******* search Error Test Passed *******")
-        # else:
-        #     self.driver.save_screenshot(".\\Screenshots\\" + "test_SearchError_scr.png")
-        #     self.logger.error("******* search Error test failed ********")
-        #     assert True == False
+        self.alert_obj = self.driver.switch_to.alert
+        self.err = self.alert_obj.text
+        self.alert_obj.accept()
+        print(self.err)
+        if "Please enter some search keyword" in self.err:
+            assert True == True
+            self.logger.info("******* search Error Test Passed *******")
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_SearchError_scr.png")
+            self.logger.error("******* search Error test failed ********")
+            assert True == False
 
 
-        # self.driver.close()
+        self.driver.close()
         self.logger.info("********* Ending Test_Negative015_SearchError ********")
