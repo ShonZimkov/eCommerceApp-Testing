164014a622c3cf35bb4155b0fa645b5a8ead8c87
shon <shoniki951@gmail.com>
2024-01-02 09:35:18 +0200

    run.bat for jenkins

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-pytest -s -v -m sanity --html=Reports\report.html testCasesAdmin/ --browser chrome
-pytest -s -v -m sanity --html=Reports\report.html testCasesUser/ --browser chrome
-pytest -s -v -m sanity --html=Reports\report.html testNegativeAdmin/ --browser chrome
-pytest -s -v -m sanity --html=Reports\report.html testNegativeUser/ --browser chrome
+pytest -s -v --html=Reports\report.html testCasesAdmin/ --browser chrome
+pytest -s -v --html=Reports\report.html testCasesUser/ --browser chrome
+pytest -s -v --html=Reports\report.html testNegativeAdmin/ --browser chrome
+pytest -s -v --html=Reports\report.html testNegativeUser/ --browser chrome
